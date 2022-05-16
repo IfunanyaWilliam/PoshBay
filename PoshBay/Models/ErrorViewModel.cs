6d00918b314a8db94e8ec4d1a474fac1edefb6b3
IfunanyaWilliam <williamifunanya@gmail.com>
2022-05-16 16:36:31 +0100

    Create a project PoshBay - an eCommerce website for groceries

---
+++ PoshBay/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PoshBay.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
